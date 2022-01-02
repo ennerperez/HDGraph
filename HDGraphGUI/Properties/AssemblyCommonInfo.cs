c5a3b2d7792617a0b534274c9ce0b5f29032a7ae
Enner Pérez <ennerperez@gmail.com>
2022-01-02 13:27:56 -0500

    Initial Commit

---
+++ HDGraphGUI/Properties/AssemblyCommonInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+
+
+[assembly: AssemblyCompany("HDGraph.com")]
+[assembly: AssemblyProduct("HDGraph")]
+[assembly: AssemblyCopyright("Copyright © 2006-2015 LAUGEL Jean-Yves")]
+[assembly: AssemblyTrademark("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.5.1.0")]
